3f58385653afa7055247064b803f78ece94ea719
SergeiBelkin <sereginsergej830@gmail.com>
2023-04-15 20:22:53 +0300

    DZ

--- treepracticC/Program.cs
+++ treepracticC/Program.cs
@@ -1,80 +1,33 @@
-﻿// **вторая задача**
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. (палиндром - число читается слева направо и справа налево одинаково)
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число:");
+string num = Console.ReadLine();
+int i = num.Length;
 
 
-// Console.WriteLine("введте число a:");
-// string text = Console.ReadLine();
-// int a = Convert.ToInt32(text);
-// Console.WriteLine("введте число b:");
-// string text2 = Console.ReadLine();
-// int b = Convert.ToInt32(text2);
+if ( i < 5 )
+{
+    Console.WriteLine("мало чисел");    
+}
 
-// if (a > b) 
-//     Console.WriteLine("a higer");
-// else
-// Console.WriteLine("b higer");
+if ( i > 5 )
+{
+    Console.WriteLine("много чисел");
+}
+int a = num[0];
+int b = num[1];
+int c = num[3];
+int d = num[4];
 
-// **четвертая задача**
-
-
-// Console.WriteLine("Enter a");
-
-// string entera = Console.ReadLine();
-// int a = Convert.ToInt32(entera);
-
-// Console.WriteLine("Enter b");
-
-// string enterb = Console.ReadLine();
-// int b = Convert.ToInt32(enterb);
-
-// Console.WriteLine("Enter c");
-
-// string enterc = Console.ReadLine();
-// int c = Convert.ToInt32(enterc);
-// int max = a;
-// if (b>a) max = b;
-// if (c>b) max = c;
-
-// Console.WriteLine("максимальное число:");
-// Console.Write(max);
-
-// **шестая задача**
-
-// Console.WriteLine("введте число:");
-// string text = Console.ReadLine();
-// int a = Convert.ToInt32(text);
-// double x = a%2;
-//     if (x == 1)
-//     {
-//     Console.WriteLine("нечетное");
-//     }
-//     else
-//     {
-//     Console.WriteLine("четное");
-//     }
-
-// **восьмая задача**
-
-Console.WriteLine("введте число:");
-string text = Console.ReadLine();
-int a = Convert.ToInt32(text);
-int index = 2;
-if(a > 0)    
-    {
-    while (index <= a)
-    {
-    Console.WriteLine(index);
-    index = index + 2;
-    }
-    }
-else 
-    {
-    index = -2;
-    
-
-    while (index >= a)
-    {
-    Console.WriteLine(index);
-    index = index - 2;
-
-    }
-    }
+if ( a == d && b==c )
+{
+        Console.WriteLine( "палиндром" );
+}    
+if (i == 5)
+{
+Console.WriteLine( "не палиндром" );
+}
